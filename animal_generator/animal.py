ed0ab31f1230de6d29014347cde75de847fc9c0b
fsousaprog <fms007@hotmail.com>
2023-02-02 17:10:24 -0300

    Formatting animal_generator with best practices

--- animal_generator/animal.py
+++ animal_generator/animal.py
@@ -2,33 +2,40 @@ from abc import ABC, abstractmethod
 from tkinter import messagebox
 
 
+# Base class for Animals with necessary attributes and abstract methods
 class Animal(ABC):
-    name = ""
-    color = ""
+    name: str = ""
+    color: str = ""
 
-    def __init__(self, name, color):
+    # Constructor with necessary attributes
+    def __init__(self, name: str, color: str):
         self.name = name
         self.color = color
         super().__init__()
 
+    # Each instance needs to implement its own
     @abstractmethod
     def make_sound(self):
         pass
 
+    # Describes the info of the animal
     def describe(self):
         messagebox.showinfo(self.name, self.color)
 
 
+# Underwater animal
 class Fish(Animal):
     def make_sound(self):
         messagebox.showinfo(self.name, "Blub blub")
 
 
+# Flying animal
 class Bird(Animal):
     def make_sound(self):
         messagebox.showinfo(self.name, "Piu piu piu")
 
 
+# Ground animal
 class Terrestrial(Animal):
     def make_sound(self):
         messagebox.showinfo(self.name, "Rawr!")
