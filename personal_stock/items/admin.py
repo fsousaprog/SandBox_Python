a600f64c16698ef3699bd514f153fdafcf19e10e
fsousaprog <fms007@hotmail.com>
2023-06-22 16:27:55 -0300

    Django Syntax on htmls

--- personal_stock/items/admin.py
+++ personal_stock/items/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
+from .models import Item
+
+
+class ItemAdmin(admin.ModelAdmin):
+    list_display = ("name", "quantity", "avg_price")
+
+
 # Register your models here.
+admin.site.register(Item, ItemAdmin)
--- personal_stock/items/models.py
+++ personal_stock/items/models.py
@@ -5,3 +5,7 @@ class Item(models.Model):
     name = models.CharField(max_length=255)
     quantity = models.IntegerField()
     avg_price = models.FloatField()
+
+
+def __str__(self):
+    return f"{self.name} Qtd: {self.quantity}"
